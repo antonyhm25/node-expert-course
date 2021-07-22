0c099ba48e3c20728e0f6c44bb9b597733754516
Antony HM <antonio_hm25@hotmail.com>
2021-07-21 21:59:41 -0500

    configurar argumentos en yargs b(requeridos) y l(opcionales), con validaciones

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,27 @@
 const { createMultiplicationFile } = require('./helpers/multiplication');
-const argv = require('yargs').argv
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true
+    })
+    .option('l', {
+        alias: 'list',
+        type: 'boolean',
+        default: false,
+        demandOption: true
+    })
+    .check((argv, options) => {
+        if (isNaN(argv.b)) {
+            throw 'La base debe ser númerico.';
+        }
 
-console.log(process.argv);
+        return true;
+    })
+    .argv;
+
 console.log(argv);
 
-console.log(argv.base);
-
-// createMultiplicationFile(base)
-//     .then(fileName => console.log(fileName, 'creado'))
-//     .catch(err => console.log(err));
+createMultiplicationFile(argv.b, argv.l)
+    .then(fileName => console.log(fileName, 'creado'))
+    .catch(err => console.log(err));
--- 03-bases-node/helpers/multiplication.js
+++ 03-bases-node/helpers/multiplication.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 
-const createMultiplicationFile = async (base = 1) => {
+const createMultiplicationFile = async (base = 1, list = false) => {
     let output = '';
 
     try {
-        console.log('========================');
-        console.log('    Tabla del:', base);
-        console.log('========================');
-
         for (let i = 1; i <= 10; i++) {
             output += `${base} x ${i} = ${base * i} \n`;
         }
 
-        console.log(output);
+        if (list) {
+            console.log('========================');
+            console.log('    Tabla del:', base);
+            console.log('========================');
+            console.log(output);
+        }
 
         const fileName = `tabla-${base}.txt`;
         fs.writeFileSync(fileName, output);
