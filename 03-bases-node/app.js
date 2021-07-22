97bf57312aa86367ee3bb34d2a823cbd41882f6a
Antony HM <antonio_hm25@hotmail.com>
2021-07-21 19:30:31 -0500

    leer desde linea de argumentos v1

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,7 +2,8 @@ const { createMultiplicationFile } = require('./helpers/multiplication');
 
 console.clear();
 
-const base = 4;
+const [,, arg3 = 'base=1'] = process.argv;
+const [, base = 1] = arg3.split('=');
 
 createMultiplicationFile(base)
     .then(fileName => console.log(fileName, 'creado'))
